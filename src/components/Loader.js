0cd1a5932ef3c3c102c463e26c758f390288becc
AbhishekEV <abhishekev007@gmail.com>
2025-05-29 22:56:44 +0530

    removing spinner

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,19 +1,10 @@
 import React from 'react'
-import { ColorRing } from 'react-loader-spinner'
 
 function Loader() {
 
   return (
     <div className="backdrop">
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="color-ring-loading"
-        wrapperStyle={{}}
-        wrapperClass="color-ring-wrapper"
-        colors={['#EA0486', '#08B3C0', '#DD1B4E', '#74BE3C', '#BC55A4']}
-      />
+      Loading...
     </div>
   )
 }
